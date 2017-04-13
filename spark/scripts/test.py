7cfd309c15d0d8647f945669b6f085f3f94d2c1b
hongchhe <hongchhe@cisco.com>
2017-04-13 08:38:43 +0800

    add spark data for test

---
+++ spark/scripts/test.py
@@ -0,0 +1,10 @@
+from pyspark import SparkFiles
+path = os.path.join(tempdir, "test.txt")
+with open(path, "w") as testFile:
+   _ = testFile.write("100")
+sc.addFile(path)
+def func(iterator):
+   with open(SparkFiles.get("test.txt")) as testFile:
+       fileVal = int(testFile.readline())
+       return [x * fileVal for x in iterator]
+sc.parallelize([1, 2, 3, 4]).mapPartitions(func).collect()

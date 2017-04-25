d6eef4e64d1f9dbc28a4f07a956ad94800abd785
hongchhe <hehongchuang@hotmail.com>
2017-04-25 15:43:51 +0800

    update hadoop with more flexible.
    1. change hadoop into /opt/hadoop
    2. put the default HADOOP_OPTS parameter
    3. provide a volume of /myvol to store the shared authorized_keys.
    4. parameterize the config files
    5. provide singleNode mode and clusterNode mode(1nn-2dn) docker-compose
    xml to be used.

--- hadoop/run.sh
+++ hadoop/run.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+: ${HDFS_HOST:=localhost}
+
+# split each slave using "\n". For example: lh1 \nlh2 
+: ${SLAVE_LIST:=localhost}
+
+
+sed -i ${HADOOP_CONF_DIR}/core-site.xml -e "s/{{hdfsHost}}/${HDFS_HOST}/"
+sed -i ${HADOOP_CONF_DIR}/mapred-site.xml -e "s/{{hdfsHost}}/${HDFS_HOST}/"
+sed -i ${HADOOP_CONF_DIR}/yarn-site.xml -e "s/{{hdfsHost}}/${HDFS_HOST}/"
+
+echo -e ${SLAVE_LIST} > ${HADOOP_CONF_DIR}/slaves
+
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> /myvol/authorized_keys
+mv $HADOOP_CONF_DIR/ssh_config ~/.ssh/config 
+
+#start ssh service
+service ssh start 
+
+#waitting for the id_rsa.pub of all nodes has been added into the /myvol/authorized_keys file
+sleep 3
+
+cp /myvol/authorized_keys ~/.ssh/authorized_keys
+
 # format namenode
 hdfs namenode -format
 
-#start ssh service
-service ssh start
-
 #start hdfs
 start-dfs.sh
+
+#start yarn
+start-yarn.sh
 
 tail -f /dev/null

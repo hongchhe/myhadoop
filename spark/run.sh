9e8807678aedd1b2dce5d8ac5420abeb83ca81f2
hongchhe <hehongchuang@hotmail.com>
2017-04-27 10:07:08 +0800

    rectify hadoop/docker-compose-cluser.yml and update the spark contents
    1. rectify hadoop/docker-compose-cluser.yml
    2. update the spark Dockerfile to add ipython-notebook for accessing
    from the external web via 8888 port.
    3. add the docker-compose.yml to be used

--- spark/run.sh
+++ spark/run.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+echo "export PYSPARK_PYTHON=/usr/bin/python2.7" >> ~/.bash_profile
+echo "export PYSPARK_DRIVER_PYTHON=ipython2.7" >> ~/.bash_profile
+# the notebook can be accessed using the command: ipython notebook --no-browser --ip=*
+echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook --no-browser --ip=*\"" >> ~/.bash_profile
+echo "export PATH=\$PATH:\$SPARK_HOME/bin:\$SPARK_HOME/sbin" >> ~/.bash_profile
+
+cat ~/.ssh/id_rsa.pub >> /myvol/authorized_keys
+
 #start ssh service
 service ssh start
 
+#waitting for the id_rsa.pub of all nodes has been added into the /myvol/authorized_keys file
+sleep 3
+cp /myvol/authorized_keys ~/.ssh/authorized_keys
+
 #start spark
 ./sbin/start-all.sh
+
+source ~/.bash_profile
 
 tail -f /dev/null

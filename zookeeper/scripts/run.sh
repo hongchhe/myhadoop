de13401c41032b6cbe5743a47b8bee65b8f7cd19
hongchhe <hehongchuang@hotmail.com>
2017-05-21 10:37:50 +0800

    update the zookeeper to support cluster env

--- zookeeper/scripts/run.sh
+++ zookeeper/scripts/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 : ${ZK_HOME:=/opt/zookeeper}
-
 : ${ZK_MODE:=standalone}
-
 : ${ZK_SERVER_NAME:=zoo}
-
 : ${ZK_SERVER_NUM:=3}
+: ${ZK_SERVER_ID:=1}
 
 cp ${ZK_HOME}/conf/zoo-${ZK_MODE}.cfg ${ZK_HOME}/conf/zoo.cfg
 
 python createServerList.py ${ZK_SERVER_NAME} ${ZK_SERVER_NUM} ${ZK_MODE} >> ${ZK_HOME}/conf/zoo.cfg
+
+echo ${ZK_SERVER_ID} > ${ZK_HOME}/var/data/myid
 
 ${ZK_HOME}/bin/zkServer.sh start
 

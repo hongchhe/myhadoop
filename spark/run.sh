309052f04698b1e06abcdb69424490b645ca20b6
hongchhe <hehongchuang@hotmail.com>
2017-05-26 17:34:55 +0800

    update the spark contents
    1. use the ubuntu-python-java as the base images
    2. add the python related library and make the ipython as the optional
    tool
    3. create the simple spark cluster env in use.
    4. update the run.sh.

--- spark/run.sh
+++ spark/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "export PYSPARK_PYTHON=/usr/bin/python2.7" >> ~/.bash_profile
-echo "export PYSPARK_DRIVER_PYTHON=ipython2.7" >> ~/.bash_profile
-# the notebook can be accessed using the command: ipython notebook --no-browser --ip=*
-echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook --no-browser --ip=*\"" >> ~/.bash_profile
-echo "export PATH=\$PATH:\$SPARK_HOME/bin:\$SPARK_HOME/sbin" >> ~/.bash_profile
+#echo "export PYSPARK_PYTHON=/usr/bin/python2.7" >> ~/.bash_profile
+#echo "export PYSPARK_DRIVER_PYTHON=ipython2.7" >> ~/.bash_profile
+## the notebook can be accessed using the command: ipython notebook --no-browser --ip=* --matplotlib
+#echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook --no-browser --ip=* --matplotlib\"" >> ~/.bash_profile
+#echo "export PATH=\$PATH:\$SPARK_HOME/bin:\$SPARK_HOME/sbin" >> ~/.bash_profile
+: ${HOST_TYPE:=master} 
+: ${SLAVE_LIST:=} 
 
 cat ~/.ssh/id_rsa.pub >> /myvol/authorized_keys
 
@@ -15,9 +17,11 @@ service ssh start
 sleep 3
 cp /myvol/authorized_keys ~/.ssh/authorized_keys
 
-#start spark
-./sbin/start-all.sh
-
-source ~/.bash_profile
+#start spark if it's master node
+if [ $HOST_TYPE="master" ]
+then
+  echo -e ${SLAVE_LIST} > ${SPARK_CONF_DIR}/slaves;
+  ./sbin/start-all.sh;
+fi
 
 tail -f /dev/null

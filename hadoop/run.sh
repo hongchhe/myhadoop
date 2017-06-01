b54c2b115420965768b8271909a310e9294f0fb2
hongchhe <hehongchuang@hotmail.com>
2017-06-01 09:04:31 +0800

    update the hadoop
    1. use hongchhe/ubuntu-python-java as base image
    2. add more parameters into hdfs-site.xml to be updated.
    3. update docker-compose yaml files and run.sh file

--- hadoop/run.sh
+++ hadoop/run.sh
@@ -5,8 +5,15 @@
 # split each slave using "\n". For example: lh1 \nlh2 
 : ${SLAVE_LIST:=localhost}
 
+# HDFS types: namenode, checkpoint, backup, datanode
+: ${HDFS_TYPE:=all}
+
+# YARN types: resourcemanager, nodemanager, webappproxy
+: ${YARN_TYPE:=all}
+
 
 sed -i ${HADOOP_CONF_DIR}/core-site.xml -e "s/{{hdfsHost}}/${HDFS_HOST}/"
+sed -i ${HADOOP_CONF_DIR}/hdfs-site.xml -e "s/{{hdfsHost}}/${HDFS_HOST}/"
 sed -i ${HADOOP_CONF_DIR}/mapred-site.xml -e "s/{{hdfsHost}}/${HDFS_HOST}/"
 sed -i ${HADOOP_CONF_DIR}/yarn-site.xml -e "s/{{hdfsHost}}/${HDFS_HOST}/"
 
@@ -24,13 +31,36 @@ sleep 3
 
 cp /myvol/authorized_keys ~/.ssh/authorized_keys
 
-# format namenode
-hdfs namenode -format
+#start the related-type hdfs
+if [ $HDFS_TYPE = "namenode" ]; then
+	# format namenode
+    hdfs namenode -format
+	#hdfs namenode;
+	hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode
+elif [ $HDFS_TYPE = "backup" ]; then
+    hdfs namenode -backup;
+elif [ $HDFS_TYPE = "checkpoint" ]; then
+    hdfs namenode -checkpoint;
+elif [ $HDFS_TYPE = "datanode" ]; then
+    hdfs datanode;
+elif [ $HDFS_TYPE = "all" ]; then
+    #start hdfs integrated with namenode, secondary namenode, datanode
+    start-dfs.sh
+else
+    echo "Do nothing for hdfs service."
+fi
 
-#start hdfs
-start-dfs.sh
-
-#start yarn
-start-yarn.sh
+#start the related-type yarn service.
+if [ $YARN_TYPE = "resourcemanager" ]; then
+	yarn-daemon.sh --config $YARN_CONF_DIR  start resourcemanager
+elif [ $YARN_TYPE = "nodemanager" ]; then
+	yarn-daemons.sh --config $YARN_CONF_DIR  start nodemanager
+elif [ $YARN_TYPE = "nodemanager" ]; then
+	yarn-daemon.sh --config $YARN_CONF_DIR  start proxyserver
+elif [ $YARN_TYPE = "all" ]; then
+	start-yarn.sh
+else
+	echo "Do nothing for yarn service."
+fi
 
 tail -f /dev/null

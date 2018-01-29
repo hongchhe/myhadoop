30ba9215c525c780eac05deaaa9031b8484c54ee
hehongchuang <hehongchuang@hotmail.com>
2018-01-29 22:50:15 +0800

    update the ZK_SERVER_ID variable.

--- zookeeper/scripts/run.sh
+++ zookeeper/scripts/run.sh
@@ -20,6 +20,11 @@ cp ${ZK_HOME}/conf/zoo-${ZK_MODE}.cfg ${ZK_HOME}/conf/zoo.cfg
 
 python createServerList.py ${ZK_SERVER_NAME} ${ZK_SERVER_NUM} ${ZK_MODE} >> ${ZK_HOME}/conf/zoo.cfg
 
+serverId=`hostname | egrep -o '[0-9]*$'`
+if [[ $serverId != "" ] && [ ${ZK_SERVER_ID} = "1" ]] ;then
+  ZK_SERVER_ID=$serverId
+fi
+
 echo ${ZK_SERVER_ID} > ${ZK_HOME}/var/data/myid
 
 ${ZK_HOME}/bin/zkServer.sh start

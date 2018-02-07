37f9fe8ecb9ff678a3fbc5410cf41ff3f7b4df74
hehongchuang <hehongchuang@hotmail.com>
2018-02-07 20:47:46 +0800

    update the zookeeper images

--- zookeeper/scripts/createServerList.py
+++ zookeeper/scripts/createServerList.py
@@ -4,7 +4,8 @@ import argparse
 parser = argparse.ArgumentParser(prog='serverList')
 parser.add_argument('serverName', help='input the zookeeper server general name')
 parser.add_argument('serverNum', type=int, help='input the zookeeper servers number')
-parser.add_argument('zkMode', nargs='?', help='input the zookeeper server configuration mode, standalone or replicated')
+parser.add_argument('--zkMode', nargs='?', help='input the zookeeper configuration mode, standalone or replicated')
+parser.add_argument('--serviceName', nargs='?', help='input the zookeeper service name customized by k8s')
 args = parser.parse_args()
 
 
@@ -12,7 +13,11 @@ def main():
     if args.zkMode is not None and args.zkMode != "standalone":
         serverName = args.serverName if args.serverName is not None else "zoo"
         num = args.serverNum if args.serverNum is not None else 0
-        serverList = ["server.{0}={1}{0}:2888:3888".format(i, serverName) for i in range(1, num + 1)]
+        if (args.serviceName is not None and args.serviceName.strip() != ""):
+            serviceName = ".{0}".format(args.serviceName.strip())
+        else:
+            serviceName = ""
+        serverList = ["server.{0}={1}-{0}{2}:2888:3888".format(i, serverName, serviceName) for i in range(0, num)]
         for item in serverList:
             print item
 
--- zookeeper/scripts/run.sh
+++ zookeeper/scripts/run.sh
@@ -6,26 +6,23 @@
 : ${ZK_SERVER_NUM:=3}
 : ${ZK_SERVER_ID:=1}
 
-
-ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub >> /myvol/authorized_keys
-mv ${ZK_HOME}/conf/ssh_config ~/.ssh/config
-
-#start ssh service
-service ssh start 
-
-ln -sf /myvol/authorized_keys ~/.ssh/authorized_keys
-
 cp ${ZK_HOME}/conf/zoo-${ZK_MODE}.cfg ${ZK_HOME}/conf/zoo.cfg
 
-python createServerList.py ${ZK_SERVER_NAME} ${ZK_SERVER_NUM} ${ZK_MODE} >> ${ZK_HOME}/conf/zoo.cfg
+python createServerList.py ${ZK_SERVER_NAME} ${ZK_SERVER_NUM} --zkMode ${ZK_MODE} \
+    --serviceName ${ZK_SERVICE_NAME} >> ${ZK_HOME}/conf/zoo.cfg
 
 serverId=`hostname | egrep -o '[0-9]*$'`
-if [[ $serverId != "" ] && [ ${ZK_SERVER_ID} = "1" ]] ;then
-  ZK_SERVER_ID=$serverId
+if [[ ${serverId} != "" && ${ZK_SERVER_ID} = "1" ]] ;then
+  ZK_SERVER_ID=${serverId}
 fi
 
-echo ${ZK_SERVER_ID} > ${ZK_HOME}/var/data/myid
+if [ ${ZK_MODE} = "replicated" ]; then
+  sed -i ${ZK_HOME}/conf/zoo.cfg -e "s/{{SERVER_ID}}/${ZK_SERVER_ID}/g"
+  mkdir -p ${ZK_HOME}/var/data-${ZK_SERVER_ID}
+  echo ${ZK_SERVER_ID} > ${ZK_HOME}/var/data-${ZK_SERVER_ID}/myid
+else
+  echo ${ZK_SERVER_ID} > ${ZK_HOME}/var/data/myid
+fi
 
 ${ZK_HOME}/bin/zkServer.sh start
 
